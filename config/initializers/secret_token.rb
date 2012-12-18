ed205cd2c34654168fbec2b6f5ab4e19f0253d5a
ButuzGOL <ButuzGOL@gmail.com>
2012-12-18 08:16:31 +0200

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bummer::Application.config.secret_token = '21772daca7087da12ea245603710b2f735d88e10231f2f7d27fcb5ff8d2a012f7dbccc51830b0ec7b889f8c7ef9142c8a6a73cf2b1a608323efe4e34c7d1dd8c'
